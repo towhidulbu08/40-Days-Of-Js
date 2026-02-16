ae59991a56902edf827c2893410667bd878f6e07
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-02-16 20:49:29 +0600

    Start Day-02

---
+++ Day-02/index.js
@@ -0,0 +1,3 @@
+console.log("DAY 02");
+
+// Variable is a container or strorage for data. It is used to store and manipulate data in a program. In JavaScript, we can declare variables using the `var`, `let`, or `const` keywords.For example: A Box is a variable that can hold different items. We can put a toy in the box, then take it out and put a book in it. The box is like a variable that can store different values.
