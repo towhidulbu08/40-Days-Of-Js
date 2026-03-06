7efeb52a5ea883e994c86c74e251581d1ff08dc9
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-03-06 08:12:58 +0600

    Completed for loops, nested loops, break, continue

--- Day-05/index.js
+++ Day-05/index.js
@@ -1,2 +1,20 @@
 // Day-05 JavaScript file
-console.log("Day-05 JavaScript file loaded");
+
+// Additon of even numbers between 1 to 100
+
+//?Nested Loops:
+
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     console.log("Row", i, "Col", j);
+//   }
+// }
+
+//?Break and Continue:
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
